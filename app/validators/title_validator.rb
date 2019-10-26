7562cb371a3beada93901094ecace74437072577
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-10-26 15:24:33 +0000

    Automatically backed up by Learn

--- app/validators/title_validator.rb
+++
@@ -1,7 +0,0 @@
-class TitleValidator < ActiveModel::Validator
-  def validate(record)
-    unless record.title.include? %w[ 'Won\'t Believe' 'Secret' 'Top /0-9/' 'Guess' ]
-      record.errors[:title] << 'Title must be clickbait-y'
-    end 
-  end 
-end 
